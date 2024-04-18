ab2a066d63a06b9841e52fe3bbeedd61cef79714
AlphaS <ar.su0104@gmail.com>
2024-04-18 12:21:46 +0300

    add experiment

--- upgrade.sh
+++ upgrade.sh
@@ -2,5 +2,5 @@ sudo rm -rf /etc/nixos/
 sudo cp -r --copy-contents ./ /etc/nixos/
 
 cd /etc/nixos
-sudo nix flake update
-sudo nixos-rebuild switch --upgrade --flake /etc/nixos --show-trace
+sudo nix flake update --experimental-features 'nix-command flakes'
+sudo nixos-rebuild switch --upgrade --flake /etc/nixos --show-trace --experimental-features 'nix-command flakes'
