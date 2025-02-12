a92d85ff6f94077d27bd76c9a8e5d032879e31e4
AlphaS <ar.su0104@gmail.com>
2025-02-12 12:22:17 +0300

    Remove obsolete configuration file config.nix

--- config.nix
+++
@@ -1,9 +0,0 @@
-{ lib, pkgs, ... }:
-
-{
-  # Configuration name and hostname
-  # FIXME: Change this to your hostname!
-  system.configurationName = "<host_here>";
-  # NixOS version
-  system.nixosVersion = "24.11";
-}
