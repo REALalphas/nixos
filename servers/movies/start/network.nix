5f6f552441a65e19e065ffa4b382dfeb1df4ce1b
AlphaS <ar.su0104@gmail.com>
2025-04-30 22:07:42 +0300

    FIX: Add extraConfig to dhcpcd for disabling IPv6 and ARP

--- servers/movies/start/network.nix
+++ servers/movies/start/network.nix
@@ -7,6 +7,12 @@
 }:
 
 {
+  networking.dhcpcd.extraConfig = ''
+    # Disable IPv6
+    noipv6
+    # Disable ARP
+    noarp
+  '';
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
 
