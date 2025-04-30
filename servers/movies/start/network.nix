061c036446bffdfb6edb28d298451b8bd277c20c
AlphaS <ar.su0104@gmail.com>
2025-04-30 21:54:33 +0300

    FIX: Comment out NetworkManager configuration to reduce bloat for server setups

--- servers/movies/start/network.nix
+++ servers/movies/start/network.nix
@@ -14,7 +14,8 @@
 
   # Pick only one of the below networking options.
   # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  # Not recommended for servers. Too much bloat. \/
+  # networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
