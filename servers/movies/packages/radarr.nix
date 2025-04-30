1ee21b8525af5beb5d983f6b12d297e4df4c8296
AlphaS <ar.su0104@gmail.com>
2025-04-30 23:05:15 +0300

    ADD: Update Sonarr service description and modify data directory; add Radarr service configuration

---
+++ servers/movies/packages/radarr.nix
@@ -0,0 +1,26 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.system.radarr;
+in
+{
+  options.system.radarr = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Whether to enable Radarr service (a PVR for Usenet and BitTorrent users for movies)";
+  };
+
+  config = lib.mkIf cfg {
+    # Enable Radarr service
+    services.radarr = {
+      enable = true;
+      dataDir = "/mnt/web-2xl/dump/Media";
+      user = "alphas";
+    };
+  };
+}
--- servers/movies/packages/sonarr.nix
+++ servers/movies/packages/sonarr.nix
@@ -12,14 +12,14 @@ in
   options.system.sonarr = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Whether to enable Sonarr service (a PVR for Usenet and BitTorrent users)";
+    description = "Whether to enable Sonarr service (a PVR for Usenet and BitTorrent users for TV shows)";
   };
 
   config = lib.mkIf cfg {
     # Enable Sonarr service
     services.sonarr = {
       enable = true;
-      dataDir = "/mnt/web-2xl/dump/Media/Services/sonarr";
+      dataDir = "/mnt/web-2xl/dump/Media";
       user = "alphas";
     };
   };
