6e2f2d72382672b6b964efcc0a0448194709ab3a
AlphaS <ar.su0104@gmail.com>
2025-05-18 20:35:27 +0300

    Update podman-compose.nix

--- servers/movies/custom/podman-compose.nix
+++ servers/movies/custom/podman-compose.nix
@@ -1,151 +1,292 @@
 { ... }:
 let
-  containers = {
-    recyclarr = {
-      image = "ghcr.io/recyclarr/recyclarr";
-      user = "1000";
-      autoStart = true;
-      ports = [ ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/recyclarr:/config"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        RECYCLARR_CREATE_CONFIG = "true";
-      };
-      dependsOn = [
-        "sonarr-shows"
-        "sonarr-anime"
-        "radarr"
-      ];
+  _ = { };
+
+  # Torrent Client
+  _.deluge = {
+    image = "lscr.io/linuxserver/deluge";
+    ports = [
+      "8112:8112"
+      "6881:6881"
+      "6881:6881/udp"
+      "58846:58846"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/deluge:/config"
+      "/mnt/web-2xl/dump/Media/data/torrents:/data/torrents"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      DELUGE_LOGLEVEL = "error";
     };
-    jellyfin = {
-      image = "jellyfin/jellyfin";
-      user = "1000";
-      autoStart = true;
-      ports = [
-        "8096:8096"
-        "8920:8920"
-        "1900:1900"
-        "7359:7359"
-      ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/jellyfin/config:/config"
-        "/mnt/web-2xl/dump/Media/.services/jellyfin/cache:/cache"
-        "/mnt/web-2xl/dump/Media:/media:ro"
-      ];
-      environment = { };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+    # extraOptions = [ "--network=host" ];
+  };
+  # Torrent Client
+  _.qbittorrent = {
+    image = "lscr.io/linuxserver/qbittorrent";
+    ports = [
+      "8080:8080"
+      "6882:6882"
+      "6882:6882/udp"
+      "7656:7656"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/qbittorrent:/config"
+      "/mnt/web-2xl/dump/Media/data/torrents:/data/torrents"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      WEBUI_PORT = "8080";
+      TORRENTING_PORT = "6882";
     };
-    jellyseerr = {
-      image = "ghcr.io/fallenbagel/jellyseerr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "5055:5055" ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/jellyseerr:/app/config"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        LOG_LEVEL = "debug";
-      };
-      dependsOn = [ "jellyfin" ];
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+    # extraOptions = [ "--network=host" ];
+  };
+  # Usenet Client
+  _.nzbget = {
+    image = "nzbgetcom/nzbget";
+    ports = [
+      "6789:6789"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/nzbget:/config"
+      "/mnt/web-2xl/dump/Media/data/usenet:/data/usenet"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      NZBGET_USER = "nzbget";
+      NZBGET_PASS = "vxa3zywg";
     };
-    deluge = {
-      image = "lscr.io/linuxserver/deluge";
-      user = "1000";
-      autoStart = true;
-      ports = [
-        "8112:8112"
-        "6881:6881"
-        "6881:6881/udp"
-      ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/deluge:/config"
-        "/mnt/web-2xl/dump/Media/torrents:/downloads"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        PUID = "1000";
-        PGID = "100";
-      };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+    # extraOptions = [ "--network=host" ];
+  };
+  # Soulseek Client
+  _.slskd = {
+    image = "slskd/slskd";
+    ports = [
+      "5030:5030"
+      "5031:5031"
+      "50300:50300"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/slskd:/app"
+      "/mnt/web-2xl/dump/Media/data/slskd:/data/slskd"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      SLSKD_REMOTE_CONFIGURATION = "true";
     };
-    sonarr-shows = {
-      image = "lscr.io/linuxserver/sonarr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "8989:8989" ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/sonarr-shows:/config"
-        "/mnt/web-2xl/dump/Media/tv:/tv"
-        "/mnt/web-2xl/dump/Media/torrents/tv:/downloads"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        PUID = "1000";
-        PGID = "100";
-      };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Sync Manager from Lidarr to slskd
+  _.soularr = {
+    image = "mrusse08/soularr";
+    ports = [ ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/soularr:/data"
+      "/mnt/web-2xl/dump/Media/data/slskd/downloaded:/data/slskd/downloaded"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      SCRIPT_INTERVAL = "300";
     };
-    sonarr-anime = {
-      image = "lscr.io/linuxserver/sonarr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "8990:8989" ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/sonarr-anime:/config"
-        "/mnt/web-2xl/dump/Media/anime:/anime"
-        "/mnt/web-2xl/dump/Media/torrents/anime:/downloads"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        PUID = "1000";
-        PGID = "100";
-      };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Shows Manager
+  _.sonarr = {
+    image = "lscr.io/linuxserver/sonarr";
+    ports = [
+      "8989:8989"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/sonarr:/config"
+      "/mnt/web-2xl/dump/Media/data:/data"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
     };
-    radarr = {
-      image = "lscr.io/linuxserver/radarr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "7878:7878" ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/radarr:/config"
-        "/mnt/web-2xl/dump/Media/movies:/movies"
-        "/mnt/web-2xl/dump/Media/torrents/movies:/downloads"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        PUID = "1000";
-        PGID = "100";
-      };
+    dependsOn = [ "qbittorrent" ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Movies Manager
+  _.radarr = {
+    image = "lscr.io/linuxserver/radarr";
+    ports = [
+      "7878:7878"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/radarr:/config"
+      "/mnt/web-2xl/dump/Media/data:/data"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
     };
-    flaresolverr = {
-      image = "ghcr.io/flaresolverr/flaresolverr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "8191:8191" ];
-      environment = {
-        TZ = "Europe/Moscow";
-        LOG_LEVEL = "info";
-        LOG_HTML = "false";
-        CAPTCHA_SOLVER = "none";
-      };
+    dependsOn = [ "qbittorrent" ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Music Manager
+  _.lidarr = {
+    image = "lscr.io/linuxserver/lidarr";
+    ports = [
+      "8686:8686"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/lidarr:/config"
+      "/mnt/web-2xl/dump/Media/data:/data"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
     };
-    prowlarr = {
-      image = "lscr.io/linuxserver/prowlarr";
-      user = "1000";
-      autoStart = true;
-      ports = [ "9696:9696" ];
-      volumes = [
-        "/mnt/web-2xl/dump/Media/.services/prowlarr:/config"
-      ];
-      environment = {
-        TZ = "Europe/Moscow";
-        PUID = "1000";
-        PGID = "100";
-      };
-      dependsOn = [ "flaresolverr" ];
+    dependsOn = [ "qbittorrent" ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Config for Sonarr/Radarr Manager
+  _.recyclarr = {
+    image = "ghcr.io/recyclarr/recyclarr";
+    ports = [ ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/recyclarr:/config"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      RECYCLARR_CREATE_CONFIG = "true";
     };
+    dependsOn = [
+      "sonarr"
+      "radarr"
+    ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Indexer of Torrent and Usenet Trackers (Bigger listing)
+  _.jackett = {
+    image = "lscr.io/linuxserver/jackett";
+    ports = [
+      "9117:9117"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/jackett:/config"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      AUTO_UPDATE = "true";
+    };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Indexer of indexers and autosetup
+  _.prowlarr = {
+    image = "lscr.io/linuxserver/prowlarr";
+    ports = [
+      "9696:9696"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/prowlarr:/config"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+    };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Dashboard to request media
+  _.jellyseerr = {
+    image = "fallenbagel/jellyseerr";
+    ports = [
+      "5055:5055"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/jellyseerr:/app/config"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      LOG_LEVEL = "debug";
+    };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+  # Dashboard to watch media
+  _.jellyfin = {
+    image = "jellyfin/jellyfin";
+    ports = [ ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/jellyfin:/config"
+      "/mnt/web-2xl/dump/Media/config/jellyfin-cache:/cache"
+      "/mnt/web-2xl/dump/Media/data/media:/data/media:ro"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      LOG_LEVEL = "debug";
+    };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+    extraOptions = [ "--network=host" ];
+  };
+  # Music streaming
+  _.navidrome = {
+    image = "deluan/navidrome";
+    ports = [
+      "4533:4533"
+    ];
+    volumes = [
+      "/mnt/web-2xl/dump/Media/config/navidrome:/data"
+      "/mnt/web-2xl/dump/Media/data/media/music:/music:ro"
+    ];
+    environment = {
+      PUID = "1000";
+      PGID = "100";
+      TZ = "Europe/Moscow";
+      ND_LOGLEVEL = "debug";
+    };
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
   };
 in
 {
-  virtualisation.oci-containers.containers = containers;
+  virtualisation.oci-containers.containers = _;
 }
