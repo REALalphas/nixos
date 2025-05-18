dba7f4584f95c7462bfb215d3f65a1515818e132
AlphaS <ar.su0104@gmail.com>
2025-05-18 20:58:48 +0300

    Update Nixpkgs version in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,20 +3,12 @@
 
   inputs = {
     # nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-nstbl.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-25-05.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-24-11.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-24-05.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-23-11.url = "github:nixos/nixpkgs/nixos-23.11";
-
-    # Spicetify-Nix a Spotify mod client (Now broken) (Not Recomended)
-    # spicetify-nix = {
-    #   url = "github:Gerg-L/spicetify-nix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # Gaming related stuff for Nix and NixOS (Not Recomended)
-    # nix-gaming.url = "github:fufexan/nix-gaming";
 
     # aagl-gtk-on-nix
     aagl.url = "github:ezKEa/aagl-gtk-on-nix/release-24.11";
@@ -39,6 +31,7 @@
     inputs@{
       self,
       nixpkgs-nstbl,
+      nixpkgs-25-05,
       nixpkgs-24-11,
       nixpkgs-24-05,
       nixpkgs-23-11,
@@ -51,6 +44,18 @@
       overlays = [
         (final: prev: {
           unstable = import nixpkgs-nstbl {
+            inherit (final) system;
+            config.allowUnfree = true;
+          };
+        })
+        (final: prev: {
+          nix25-05 = import nixpkgs-25-05 {
+            inherit (final) system;
+            config.allowUnfree = true;
+          };
+        })
+        (final: prev: {
+          nix24-11 = import nixpkgs-24-11 {
             inherit (final) system;
             config.allowUnfree = true;
           };
@@ -75,10 +80,14 @@
       configName = config.system.configurationName;
       hostname = config.system.configurationName;
       nixosVersion = config.system.nixosVersion;
-      stateVersion = config.system.nixosVersion;
+      stateVersion = if nixosVersion == "unstable" then "25.05" else nixosVersion;
 
       nixpkgs =
-        if nixosVersion == "24.11" then
+        if nixosVersion == "unstable" then
+          inputs.nixpkgs-nstbl
+        else if nixosVersion == "25.05" then
+          inputs.nixpkgs-25-05
+        else if nixosVersion == "24.11" then
           inputs.nixpkgs-24-11
         else if nixosVersion == "24.05" then
           inputs.nixpkgs-24-05
