19d60650ea58e70ea0fc9c9eb4a8e36513530911
AlphaS <ar.su0104@gmail.com>
2025-05-18 20:33:47 +0300

    Add gnome-builder with flatpak-builder to make gnome apps

--- hosts/nas/1packages/11programs/development.nix
+++ hosts/nas/1packages/11programs/development.nix
@@ -1,5 +1,18 @@
-{ ... }:
+{
+  pkgs,
+  ...
+}:
 {
   # Android Debug Bridge
   programs.adb.enable = true;
+
+  # Gnome Buider
+  # Creating apps
+  environment.systemPackages = (
+    with pkgs;
+    [
+      gnome-builder
+      flatpak-builder
+    ]
+  );
 }
