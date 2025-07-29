5830d35a4d759b308171eb38e8e862c1266d9250
AlphaS <ar.su0104@gmail.com>
2025-07-30 02:02:58 +0300

    Add initial kernel boot options
    This commit adds boot options for splash screen, silencing the boot
    process, and reducing the bootloader timeout.

---
+++ packages/kernel/boot-opts.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.system.boot;
+in
+{
+  options.system.boot = {
+    splash = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to enable plymouth splash screen";
+    };
+    silence = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether to silence boot";
+    };
+    reduceTimeout = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Reduce bootloader timeout to 3 seconds";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      boot.plymouth.enable = true;
+    })
+    (lib.mkIf cfg.silence {
+      boot.consoleLogLevel = 3;
+      boot.initrd.verbose = false;
+      boot.kernelParams = [
+        "quiet"
+        "splash"
+        "boot.shell_on_fail"
+        "udev.log_priority=3"
+        "rd.systemd.show_status=auto"
+      ];
+    })
+    (lib.mkIf cfg.reduceTimeout {
+      boot.loader.timeout = 3;
+    })
+  ];
+}
