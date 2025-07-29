9c355eefedd141a50dfb41a37ecfb1aff014443c
AlphaS <ar.su0104@gmail.com>
2025-07-30 02:03:42 +0300

    Add browser configuration with provider options
    This commit adds a new system option for configuring a web browser. It
    allows users to select from several providers such as Firefox, Firefox
    Beta, Firefox ESR, Firefox Dev, and Librewolf. The configuration
    includes an option to choose between pre-compiled binaries or source
    packages. The default provider is set to "firefox".

---
+++ packages/programs/browser.nix
@@ -0,0 +1,76 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.system.browser;
+in
+{
+  options.system.browser = {
+    description = "Web Browser";
+
+    provider = lib.mkOption {
+      type = lib.types.enum [
+        "firefox"
+        "firefox-beta"
+        "firefox-esr"
+        "firefox-dev"
+        "librewolf"
+      ];
+      default = "firefox"; # Changed default to a valid provider
+      description = "Select your preferred browser option";
+    };
+
+    useBin = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to use pre-compiled binary or not";
+    };
+  };
+
+  config = lib.mkMerge [
+    (lib.mkIf
+      (lib.elem cfg.provider [
+        "firefox"
+        "firefox-beta"
+        "firefox-esr"
+        "firefox-dev"
+        "librewolf"
+      ])
+      {
+        programs.firefox = {
+          enable = true;
+          package =
+            let
+              binaryPkgs = {
+                "firefox" = pkgs.firefox-bin;
+                "firefox-beta" = pkgs.firefox-beta;
+                "firefox-esr" = pkgs.firefox-esr;
+                "firefox-dev" = pkgs.firefox-devedition;
+                "librewolf" = pkgs.librewolf-bin;
+              };
+
+              sourcePkgs = {
+                "firefox" = pkgs.firefox;
+                "firefox-beta" = pkgs.firefox-beta;
+                "firefox-esr" = pkgs.firefox-esr;
+                "firefox-dev" = pkgs.firefox-devedition;
+                "librewolf" = pkgs.librewolf;
+              };
+
+              selectedPkgs = if cfg.useBin then binaryPkgs else sourcePkgs;
+
+            in
+            selectedPkgs.${cfg.provider};
+        };
+      }
+    )
+    (lib.mkIf (cfg.provider == "chrome") {
+      # TODO: Add chrome
+      # TODO: Add KDE Check
+    })
+  ];
+}
