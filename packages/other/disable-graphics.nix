fa45a5e1b1b129f440ac06fa081a24f4da16d1ea
AlphaS <ar.su0104@gmail.com>
2025-07-30 02:03:26 +0300

    Add option to disable graphics for headless systems

---
+++ packages/other/disable-graphics.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  cfg = config.system.headless;
+in
+{
+  options.system.headless = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = "Whether to disable all graphical/X11 related services and features";
+  };
+
+  config = lib.mkIf cfg {
+    # Disable all graphical/X11 related services
+    services.xserver.enable = false;
+    services.xserver.excludePackages = with pkgs; [
+      xterm
+      xorg.xorgserver
+      xorg.xinit
+    ];
+
+    # Explicitly disable any graphics-related features
+    hardware.graphics.enable = false;
+  };
+}
