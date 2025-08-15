5a59ff1426b11b8c869b8d4733476a3dc9903106
AlphaS <ar.su0104@gmail.com>
2025-08-15 15:45:11 +0300

    Remove TLS configuration (Was internal)

--- hosts/movies/custom/snipe-it.nix
+++ hosts/movies/custom/snipe-it.nix
@@ -8,7 +8,6 @@
     appKeyFile = /etc/nixos/hosts/movies/custom/snipe-it.secret;
     # hostName = "debugsnipeit.example.invalid";
     database.createLocally = true;
-    tlsEnabled = false;
   };
 
   networking.firewall.allowedTCPPorts = [
