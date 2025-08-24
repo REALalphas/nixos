980d8e46d8c3b446b9ba9f9f6aa1e761cd47b31b
AlphaS <ar.su0104@gmail.com>
2025-08-24 21:42:56 +0300

    Add label-it container configuration for movies host

---
+++ hosts/movies/custom/label-it.nix
@@ -0,0 +1,28 @@
+{
+  pkgs,
+  ...
+}:
+
+let
+  host = "snipe.nas-2xl.ru";
+in
+{
+  environment.sessionVariables.PUPPETEER_SKIP_DOWNLOAD = 1;
+  environment.sessionVariables.PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium.outPath}/bin/chromium";
+  environment.systemPackages = [
+    pkgs.chromium
+  ];
+
+  virtualisation.oci-containers.containers.labelIt = {
+    image = "ghcr.io/realalphas/label-it:latest";
+    ports = [
+      "3001:3000"
+    ];
+    volumes = [
+      "/home/alphas/labelit_dotenv:/app/.env"
+    ];
+    dependsOn = [ ];
+    user = "1000";
+    autoStart = true;
+  };
+}
